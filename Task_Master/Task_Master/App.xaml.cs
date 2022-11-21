57dde76e23c3da68b1d18f7d2987ed499cf1da83
eochistyakov <eochistyakov@qp.ru>
2022-11-21 12:11:06 +0300

    Добавьте файлы проекта.

---
+++ Task_Master/Task_Master/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Task_Master.Services;
+using Task_Master.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Task_Master
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
